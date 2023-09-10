a54d00798c09ee4c9eb818e28e268f752a2c1875
Ashfaqur Rahman <ashfaqur.rahman1@gmail.com>
2023-09-10 17:56:36 +0200

    log file with timestamps

--- src/capture/security_cam.py
+++ src/capture/security_cam.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from util import get_log_file_name
 from argparse import ArgumentParser
 
 logger = logging.getLogger(__name__)
@@ -18,29 +19,29 @@ def args_parser() -> ArgumentParser:
     arg_parser.add_argument(
         "-v", "--verbose", action="store_true", help="Set the log level to DEBUG"
     )
-    arg_parser.add_argument("--logpath", type=str, help="Path to the log file")
+    arg_parser.add_argument("--logdir", type=str, help="Path to the log directory")
     return arg_parser
 
 
-def set_log_config(verbose: bool, logpath: str) -> None:
+def set_log_config(verbose: bool, logdir: str) -> None:
     log_level: int = logging.INFO
     log_level_detail: str = "INFO"
     if verbose:
         log_level = logging.DEBUG
         log_level_detail = "DEBUG"
-    if logpath:
-        dir_name = os.path.dirname(logpath)
-        if os.path.exists(dir_name):
+    if logdir:
+        if os.path.isdir(logdir):
+            log_file_path = os.path.join(logdir, get_log_file_name())
             logging.basicConfig(
-                filename=logpath,
+                filename=log_file_path,
                 filemode="w",
                 format="%(asctime)s %(levelname)-8s %(message)s",
                 level=log_level,
                 datefmt="%Y-%m-%d %H:%M:%S",
             )
-            logging.info(f"Logs are written to file {logpath}")
+            logging.info(f"Logs are written to file '{log_file_path}'")
         else:
-            raise ValueError(f"Given log file path '{logpath}' is invalid")
+            raise ValueError(f"Given log file directory '{logdir}' is invalid")
     else:
         logging.basicConfig(
             format="%(asctime)s %(levelname)-8s %(message)s",
@@ -53,4 +54,4 @@ def set_log_config(verbose: bool, logpath: str) -> None:
 if __name__ == "__main__":
     parser: ArgumentParser = args_parser()
     args = parser.parse_args()
-    set_log_config(args.verbose, args.logpath)
+    set_log_config(args.verbose, args.logdir)
