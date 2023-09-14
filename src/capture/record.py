77f5a6694703de9ec5aaa662522c421610f88a3b
Ash <ashfaqur.rahman1@gmail.com>
2023-09-14 09:26:18 +0200

    infinite video fram loop

--- src/capture/record.py
+++ src/capture/record.py
@@ -1,6 +1,6 @@
 import logging
 import cv2
-from typing import Any
+from typing import Any, Optional
 
 VIDEO_DEVICE_ID = 0
 
@@ -8,11 +8,37 @@ logger = logging.getLogger(__name__)
 
 
 def main() -> None:
-    logger.info("main")
     cap = cv2.VideoCapture(VIDEO_DEVICE_ID)
     check_camera_open(cap)
+    try:
+        recording(capture=cap, show_window=False, crop_frame=None)
+    except KeyboardInterrupt:
+        logger.warn("Stopping script with keyboard interrupt")
+    finally:
+        # Release handle to the camera
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+def recording(
+    capture: Any,
+    show_window: bool = False,
+    crop_frame: Optional[tuple[int, int, int, int]] = None,
+) -> None:
+    while True:
+        _, frame = capture.read()
+        if crop_frame and len(crop_frame) == 4:
+            frame = frame[crop_frame[0] : crop_frame[1], crop_frame[2] : crop_frame[3]]
+        height, width = frame.shape[:2]
+
+        if show_window:
+            cv2.imshow("Frame", frame)
+            if cv2.waitKey(1) == ord("q"):
+                break
 
 
 def check_camera_open(cap: Any) -> None:
     if cap is None or not cap.isOpened():
         raise ValueError(f"Unable to open video source at '{VIDEO_DEVICE_ID}'")
+    else:
+        logging.debug(f"Using video source at id '{VIDEO_DEVICE_ID}'")
