f851d8583695f0b569f5db940afa5659ba7eceb2
Shahanaz Karim <shahnaz.karim101@gmail.com>
2023-11-21 11:10:10 +0600

    Remove outdated test

--- tests/security_cam_test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from src.capture.security_cam import set_log_config
-from src.capture.security_cam import args_parser
-
-
-def test_set_log_config() -> None:
-    # Test with invalid log file path
-    with pytest.raises(ValueError):
-        set_log_config(True, "/invalid/path/to/logfile")
