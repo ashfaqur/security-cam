5e11db06c205cab3d1cce1193f6b520143fd0101
Shahanaz Karim <shahnaz.karim101@gmail.com>
2023-12-12 23:42:04 +0600

    update scripts

--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/pi/sb/security-cam
-/home/pi/sb/security-cam/.venv/bin/python -m src.capture.security_cam /home/pi/Pictures -v -d /home/pi/sb/Dropbox-Uploader/dropbox_uploader.sh -l /home/pi/sb/security-cam/logs
