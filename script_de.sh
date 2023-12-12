18b887f59dae6423deebde9671aa48274c48599e
Shahanaz Karim <shahnaz.karim101@gmail.com>
2023-12-12 23:48:19 +0600

    update scripts

--- script_de.sh
+++ script_de.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-cd /home/pi/sb/security-cam
-/home/pi/sb/security-cam/.venv/bin/python -m src.capture.security_cam /home/pi/Pictures -v -d /home/pi/sb/Dropbox-Uploader/dropbox_uploader.sh -l /home/pi/sb/security-cam/logs -u de/images -c "110,320,220,440"
+
+while true; do
+    cd /home/pi/sb/security-cam
+    if pgrep -f "src.capture.security_cam" > /dev/null
+    then
+        echo "Running"
+    else
+        echo "Stopped, Restarting"
+        /home/pi/sb/security-cam/.venv/bin/python -m src.capture.security_cam /home/pi/Pictures -v -d /home/pi/sb/Dropbox-Uploader/dropbox_uploader.sh -u de/images -l /home/pi/sb/security-cam/logs &
+    fi
+    sleep 5
+done
